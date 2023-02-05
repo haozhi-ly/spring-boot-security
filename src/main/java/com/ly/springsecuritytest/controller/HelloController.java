ae47fac4c94bceff709c3d893c77252f87689672
ly <542933376@qq.com>
2023-02-05 22:14:27 +0800

    gateway

---
+++ src/main/java/com/ly/springsecuritytest/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.ly.springsecuritytest.controller;
+
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/hello/thymeleaf")
+    public  String index(String name,ModelMap modelMap, HttpSession session){
+        modelMap.addAttribute("name",name);
+        return "hello";
+    }
+}
