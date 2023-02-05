37eaa2950673f78c533ee9a2273e07805c46ca1c
ly <542933376@qq.com>
2023-02-05 22:56:21 +0800

    gateway

--- src/main/java/com/ly/springsecuritytest/SpringSecurityTestApplication.java
+++ src/main/java/com/ly/springsecuritytest/SpringSecurityTestApplication.java
@@ -1,16 +1,12 @@
 package com.ly.springsecuritytest;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityFilterAutoConfiguration;
 
-@SpringBootApplication(exclude = {SecurityFilterAutoConfiguration.class,SecurityAutoConfiguration.class,ManagementWebSecurityAutoConfiguration.class})
+@SpringBootApplication
 public class SpringSecurityTestApplication {
 
     public static void main(String[] args) {
-        System.out.println(Object.class.isAssignableFrom(SpringSecurityTestApplication.class));
         SpringApplication.run(SpringSecurityTestApplication.class, args);
     }
 
--- src/main/java/com/ly/springsecuritytest/controller/HelloController.java
+++ src/main/java/com/ly/springsecuritytest/controller/HelloController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HelloController {
 
-    @RequestMapping("/hello/thymeleaf")
+    @RequestMapping("/hello")
     public  String index(String name,ModelMap modelMap, HttpSession session){
         modelMap.addAttribute("name",name);
         return "hello";
